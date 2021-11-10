c541d66ce24277d506985ec1756205e0726dbb68
Aldous McKinney <amckinney2021@elam.co.uk>
2021-11-10 12:35:45 +0000

    Added physics based 4 directional movement + jumping Gave the player a slippery surface material Tested this using 3d objects

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -9,22 +9,41 @@ public class CharacterController : MonoBehaviour
     float camRotation = 0.0f;
     public float rotationSpeed = 2.0f;
     public float camRotationSpeed = 1.5f;
-
     GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
-        transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+        //transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
     }
 }
